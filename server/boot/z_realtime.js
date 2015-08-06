60d4d5df2a3e1783dbfb630491c297ca4bfc4cf8
Rand McKinney <rand@strongloop.com>
2015-08-06 15:43:23 -0700

    Delete z_realtime.js

--- server/boot/z_realtime.js
+++
@@ -1,8 +0,0 @@
-var es = require('event-stream');
-module.exports = function(app) {
-  var MyModel = app.models.MyModel;
-  MyModel.createChangeStream(function(err, changes) {
-    changes.pipe(es.stringify()).pipe(process.stdout);
-  });
-  MyModel.create({foo: 'bar'});
-}
